50f615e25a9caa0bd87332d3b005f843258796ad
Karan Solanki <20240526+solan117@users.noreply.github.com>
2021-08-29 13:02:33 -0400

    Integrated swagger

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const app = express();
 const fetch = require('node-fetch');
 const VPIC_API_BASE_URL = 'https://vpic.nhtsa.dot.gov/api';
 const VPIC_API_DEFAULT_FORMAT = 'json';
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+
 
 app.use(express.json());
 const port = process.env.PORT || 3000;
@@ -14,13 +17,15 @@ app.get('/', (request, response) => {
 
 app.get('/makes', async (request, response) => {
   const makeApiUrl = `${VPIC_API_BASE_URL}/vehicles/getallmakes?format=${VPIC_API_DEFAULT_FORMAT}`;
-  const fetchedMakes = await fetch(makeApiUrl);
-  const data = await fetchedMakes.json();
-  const allMakes = data.Results.map((makes) => {
+  const res = await fetch(makeApiUrl);
+  const fetchedMakes = await res.json();
+
+  const allMakes = fetchedMakes.Results.map((makes) => {
     return { makeName: makes.Make_Name };
   });
 
-  response.json(allMakes);
+  response.json({ makes: allMakes });
+
 });
 
 app.get('/models', async (request, response) => {
@@ -28,29 +33,35 @@ app.get('/models', async (request, response) => {
   const year = request.query.year;
   const modelsApiUrl = `${VPIC_API_BASE_URL}/vehicles/getmodelsformakeyear/make/${make}/modelyear/${year}?format=${VPIC_API_DEFAULT_FORMAT}`;
 
-  const fetchedModels = await fetch(modelsApiUrl);
-  const data = await fetchedModels.json();
-  const allModels = data.Results.map((model) => {
+  const res = await fetch(modelsApiUrl);
+  const fetchedModels = await res.json();
+
+  const allModels = fetchedModels.Results.map((model) => {
     return {
       modelId: model.Model_ID,
       modelName: model.Model_Name,
     };
   });
-  response.json(allModels);
+
+  response.json({ models: allModels });
+
 });
 
 app.get('/vin', async (request, response) => {
   const vin = request.query.vin;
 
   const modelsApiUrl = `${VPIC_API_BASE_URL}/vehicles/decodevinvalues/${vin}?format=${VPIC_API_DEFAULT_FORMAT}`;
-  const fetchResponse = await fetch(modelsApiUrl);
-  const data = await fetchResponse.json();
+  const res = await fetch(modelsApiUrl);
+  const fetchedVin = await res.json();
+
   response.json({
-    Make: data.Results[0].Make,
-    Model: data.Results[0].Model,
-    ModelYear: data.Results[0].ModelYear,
+    make: fetchedVin.Results[0].Make,
+    modelName: fetchedVin.Results[0].Model,
+    modelYear: fetchedVin.Results[0].ModelYear,
   });
 });
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, function () {
   console.log('listening on 3000');
